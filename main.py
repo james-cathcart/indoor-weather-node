adc8d31ec7e3991834754cd2b55a4ddce0446680
James Cathcart <cathcart.jm@gmail.com>
2023-10-06 17:16:57 -0600

    added env var value check

--- main.py
+++ main.py
@@ -15,15 +15,22 @@ master_port = os.getenv('MASTER_PORT')
 location = os.getenv('WEATHER_LOCATION')
 
 def main():
-   
-   weather_svc = WeatherService(sense, location)
-   master_svc = MasterService(master_host, master_port)
-   print('starting weather node')
 
-   while True:
-       data = weather_svc.get_data()
-       master_svc.record_data(data)
-       time.sleep(read_interval_seconds)
+    if master_host is None or master_host == '':
+        exit(1)
+    elif master_port is None or master_port == '':
+        exit(2)
+    elif location is None or location == '':
+        exit(3)
+
+    weather_svc = WeatherService(sense, location)
+    master_svc = MasterService(master_host, master_port)
+    print('starting weather node')
+
+    while True:
+        data = weather_svc.get_data()
+        master_svc.record_data(data)
+        time.sleep(read_interval_seconds)
 
 
 if __name__ == "__main__":
