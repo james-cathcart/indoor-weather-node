e0b5b1f12ca83d8fb4912b30492437800e33b05a
James Cathcart <cathcart.jm@gmail.com>
2023-10-14 11:18:10 -0600

    added log level and generalized exception handling

--- master.py
+++ master.py
@@ -3,14 +3,18 @@ import requests
 
 class MasterService:
 
-    def __init__(self, host, port):
+    def __init__(self, host, port, log_level):
         self.master_host = host
         self.master_port = port
         self.master_url = f'http://{self.master_host}:{self.master_port}/ingest/weather'
+        self.log_level = log_level
 
     def record_data(self, data):
+
+        if self.log_level == 'verbose':
+            print(f"sending data: {data}")
+
         try:
-            res = requests.post(self.master_url, json=data)
-        except Exception:
-            print('call to master server failed', res.text)
+            requests.post(self.master_url, json=data)
+        except:
             pass
